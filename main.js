ae28fdd4221fbb4ff9511ea5308d06c13b449d58
Jesus <joa@qitiandesign.cn>
2019-09-06 16:05:35 +0800

    adding support for canvas output

--- main.js
+++ main.js
@@ -32,9 +32,47 @@ function generateMarkerSvg(width, height, bits) {
 	return svg;
 }
 
-var dict;
+function generateMarkerCanvas(width, height, bits, size) {
+	var canvas = document.createElement("canvas");
+	canvas.width = (width+2) * size;
+	canvas.height = (height+2) * size;
+	var ctx = canvas.getContext("2d");
+	
+	
+	ctx.fillStyle = "black";
+	ctx.fillRect(
+		0,
+		0,
+		(width + 2) * size,
+		(height + 2) * size
+	);
+	
+	var fillRect = function(x, y, width2, height2){
+		ctx.fillRect(
+			x * size,
+			y * size,
+			width2 * size,
+			height2 * size
+		);
+	}
 
-function generateArucoMarker(width, height, dictName, id) {
+	// "Pixels"
+	for (var i = 0; i < height; i++) {
+		for (var j = 0; j < width; j++) {
+			ctx.fillStyle = bits[i * height + j] == 1 ? 'white' : 'black';
+			fillRect(
+				j+1,
+				i+1,
+				1,
+				1
+			);
+		}
+	}
+	
+	return canvas;
+}
+
+function generateArucoMarker(width, height, dictName, id, size, format) {
 	console.log('Generate ArUco marker ' + dictName + ' ' + id);
 
 	var bytes = dict[dictName][id];
@@ -48,30 +86,34 @@ function generateArucoMarker(width, height, dictName, id) {
 			bits.push((byte >> i) & 1);
 		}
 	}
-
-	return generateMarkerSvg(width, height, bits);
+	
+	if(format == "Svg")
+		return generateMarkerSvg(width, height, bits);
+	else if(format == "Canvas")
+		return generateMarkerCanvas(width, height, bits, size);
+	else
+		throw "Unknown format value: " + format;
 }
-
-var loadDict = $.getJSON('dict.json', function(data) {
-	dict = data;
-});
 
 $(function() {
 	var dictSelect = $('.setup select[name=dict]');
 	var markerIdInput = $('.setup input[name=id]');
 	var sizeInput = $('.setup input[name=size]');
+	var outputFormatSelect = $('.setup select[name=output_format]');
 
 	function updateMarker() {
+		
 		var markerId = Number(markerIdInput.val());
 		var size = Number(sizeInput.val());
 		var dictName = dictSelect.val();
+		var outputFormat = outputFormatSelect.val();
 		var width = Number(dictSelect.find('option:selected').attr('data-width'));
 		var height = Number(dictSelect.find('option:selected').attr('data-height'));
 
-		// Wait until dict data is loaded
-		loadDict.then(function() {
-			// Generate marker
-			var svg = generateArucoMarker(width, height, dictName, markerId, size);
+		// Generate marker
+		var element = generateArucoMarker(width, height, dictName, markerId, size, outputFormat);
+		if(outputFormat == "Svg"){
+			var svg = element;
 			svg.attr({
 				width: size + 'mm',
 				height: size + 'mm'
@@ -81,12 +123,23 @@ $(function() {
 				href: 'data:image/svg;base64,' + btoa(svg[0].outerHTML.replace('viewbox', 'viewBox')),
 				download: dictName + '-' + markerId + '.svg'
 			});
-			$('.marker-id').html('ID ' + markerId);
-		})
+		}
+		else if(outputFormat == "Canvas"){
+			//$(element).attr({
+			//	width: size + 'mm',
+			//	height: size + 'mm'
+			//});
+			$('.marker').html(element);
+		}
+		$('.marker-id').html('ID ' + markerId);
 	}
 
 	updateMarker();
 
 	dictSelect.change(updateMarker);
+	outputFormatSelect.change(updateMarker);
 	$('.setup input').on('input', updateMarker);
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	document.head.appendChild(script);
 });
